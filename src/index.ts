61d712fe1bcbcd454dce0d53129815c4a43ef3ad
pedro-git-projects <pedro.generic.email@gmail.com>
2022-09-17 17:20:57 -0300

    all month classes

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
-import { January } from "./months";
-import {table} from "table"; 
+import { April, February, January } from "./months";
 
 let j = new January();
 j.printMonth();
 
+let f = new February();
+f.printMonth();
+
+let a = new April();
+a.printMonth();
